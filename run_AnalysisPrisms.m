b89d906b9c3128d76c13fcf7680ea4ed5202e174
ethanknights <ethanknights@hotmail.co.uk>
2021-10-29 16:39:23 +0100

    write sideofspace + eccentricitiy factors to task_longdata.csv

--- run_AnalysisPrisms.m
+++ run_AnalysisPrisms.m
@@ -60,7 +60,7 @@ plot_OL_scatterANDerrorBar('data_openloop_wide_MouseClick1RT_mean',[250,1500],fa
 export_fig 'images/data_openloop_wide_MouseClick1RT_mean_scatterANDerrorBar.tiff' -transparent
 
 
-%% Pro-/Anti-Point
+%% Pro-/Anti-Point - Original Absolute Error
 %% ========================================================================
 do_rawData_PP_AP = true;
 if do_rawData_PP_AP
@@ -119,25 +119,8 @@ if do_rawData_PP_AP
     'Absolute Error (mm)');
 end
 
-% t_PP = readtable('csv/data_propointing.csv');
-% t_AP = readtable('csv/data_antipointing.csv');
-% 
-% 
-% [d_PP] = plot_task(t_PP,'Pro-Pointing');
-% [d_AP] = plot_task(t_AP,'Anti-Pointing');
-% close all
-% 
-% %% plot Task Difference
-% d = d_AP - d_PP;
-% d(:,3) = d_PP(:,3); %fix prism group (PP,AP same!)
-% plot_diff(abs(d),'TaskDifference_AP-PP_absolute',[-5,60],{'Pre-Prism','Post-Prism'});
-% plot_diff(d,'TaskDifference_AP-PP_directional',[-80,80], {'Pre-Prism','Post-Prism'});
-% 
-% %% plot Session Difference
-% d(:,1) = d_PP(:,2) - d_PP(:,1);
-% d(:,2) = d_AP(:,2) - d_AP(:,1);
-% d(:,3) = d_PP(:,3); %fix prism group (PP,AP same!)
-% plot_diff(abs(d),'SessionDifference_Post-Pre_absolute',[-5,60],{'Pro-point','Anti-point'});
-% plot_diff(d,'SessionDifference_Post-Pre_directional',[-80,80], {'Pro-point','Anti-point'});
-% 
-% 
+
+return 
+%% ADDITIONAL
+%% ========================================================================
+%% ========================================================================
